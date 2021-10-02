61451a5a28b29b0910de5d3eeaae8aeb9f44e884
Tsvetelin Stefanov <39906415+ProgrammingPro27@users.noreply.github.com>
2021-10-02 22:46:19 +0200

    Update contentMethods.js

--- contentMethods.js
+++ contentMethods.js
@@ -2,43 +2,42 @@ let menu = document.getElementById("menu");
 let tileOptions = document.getElementById("tileOptions");
 let mapOptions = document.getElementById("mapOptions");
 let mapOptionsWindow = document.getElementById("optionPartsMenu");
+let operations = ["removeTile", "increaseSize", "addUpperLevel", "colorise"];
 
 function closeSegment(e) {
     if (e.tagName === "DIV") {
         e.parentNode.remove();
-    }
-}
+    };
+};
 
 let mapOperations = {
     "RowsAndColumns": () => {
         let optionFragment = `<div class = "part" id ="RowsAndColumns">`;
-        optionFragment += `<label for="quantity1">Rows: </label><input type="range" id="quantity1" name="quantity1" min="1" max="50" value = "1" oninput="showValX(this)">`
-        optionFragment += `<label for="quantity2">Columns: </label><input type="range" id="quantity2" name="quantity2" min="1" max="50" value = "1" oninput="showValY(this)">`
-        optionFragment += `<div class = "closeButton" onclick="closeSegment(this)">&#10006;</div></div>`
+        optionFragment += `<label for="quantity1">Flat Map Size: </label><input type="range" id="quantity1" name="quantity1" min="1" max="50" value = "1" oninput="updateMap('c1')">`;
+        optionFragment += `<div class = "closeButton" onclick="closeSegment(this)">&#10006;</div></div>`;
         return optionFragment;
     },
     "PerlinMapRowsAndColumns": () => {
         let optionFragment = `<div class = "part" id ="PerlinMapRowsAndColumns">`;
-        optionFragment += `<label for="quantity3">Perlin Rows: </label><input  type="range" id="quantity3" name="quantity3" min="1" max="50" value = "1" oninput="performPerlinMapUpdate(0,this)">`
-        optionFragment += `<label for="quantity4">Perlin Columns: </label><input  type="range" id="quantity4" name="quantity4" min="1" max="50" value = "1" oninput="performPerlinMapUpdate(1,this)">`
-        optionFragment += `<div class = "closeButton" onclick="closeSegment(this)">&#10006;</div></div>`
+        optionFragment += `<label for="quantity3">Perlin Map Size: </label><input  type="range" id="quantity3" name="quantity3" min="1" max="50" value = "1" oninput="updateMap('c2')">`;
+        optionFragment += `<div class = "closeButton" onclick="closeSegment(this)">&#10006;</div></div>`;
         return optionFragment;
     },
     "GridSize": () => {
         let optionFragment = `<div class = "part" id ="GridSize">`;
-        optionFragment += `<label for="gridSize">Grid Size: </label><input  type="number" id="gridSize" name="gridSize" min="1" value = "8" oninput="performPerlinMapUpdate(2,this)">`;
+        optionFragment += `<label for="gridSize">Grid Size: </label><input  type="number" id="gridSize" name="gridSize" min="1" value = "8" oninput="updateMap('c2')">`;
         optionFragment += `<div class = "closeButton" onclick="closeSegment(this)">&#10006;</div></div>`;
         return optionFragment;
     },
     "Resolution": () => {
         let optionFragment = `<div class = "part" id ="Resolution">`;
-        optionFragment += `<label for="resolution">Resolution: </label><input  type="number" id="resolution" name="resolution" min="1" value = "64" oninput="performPerlinMapUpdate(3,this)">`
+        optionFragment += `<label for="resolution">Resolution: </label><input  type="number" id="resolution" name="resolution" min="1" value = "64" oninput="updateMap('c2')">`;
         optionFragment += `<div class = "closeButton" onclick="closeSegment(this)">&#10006;</div></div>`;
         return optionFragment;
     },
     "GroundLayers": () => {
         let optionFragment = `<div class = "part" id ="GroundLayers" >`;
-        optionFragment += `<label for="groundLayers">Ground Layers: </label><input  type="number" id="groundLayers" name="groundLayers" min="0.1" value = "0.5" step = "0.1" oninput="performPerlinMapUpdate(4,this)">`;
+        optionFragment += `<label for="groundLayers">Ground Layers: </label><input  type="number" id="groundLayers" name="groundLayers" min="0.3" value = "0.5" step = "0.1" oninput="updateMap('c2')">`;
         optionFragment += `<div class = "closeButton" onclick="closeSegment(this)">&#10006;</div></div>`;
         return optionFragment;
     },
@@ -48,9 +47,9 @@ window.onload = function () {
     tileOptions.addEventListener("click", addCommand);
     function addCommand(e) {
         if (e.target.tagName === "BUTTON") {
-            gameObject.eventToPut = e.target.innerHTML
-        }
-    }
+            gameObject.eventToPut = e.target.innerHTML;
+        };
+    };
     mapOptions.addEventListener("click", addCommandV2);
     function addCommandV2(e) {
         if (e.target.tagName === "BUTTON") {
@@ -58,10 +57,10 @@ window.onload = function () {
                 alert("Element has already been added.");
             } else {
                 mapOptionsWindow.innerHTML += mapOperations[e.target.innerHTML]();
-            }
-        }
-    }
-}
+            };
+        };
+    };
+};
 
 //add the menu window and assign every operation to the name of the button
 function addOptionButtons(operationGroup, menu) {
@@ -70,10 +69,10 @@ function addOptionButtons(operationGroup, menu) {
         button.className = "mainMenuOptionButtons";
         button.innerHTML = operationGroup[i];
         menu.appendChild(button);
-    }
+    };
 };
 
-addOptionButtons(gameObject.operations, tileOptions);
+addOptionButtons(operations, tileOptions);
 addOptionButtons(Object.keys(mapOperations), mapOptions);
 
 function showElement(el) {
