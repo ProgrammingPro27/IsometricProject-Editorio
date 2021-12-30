9f5a21433f2772611aa2b644666e606bec8a4e82
Tsvetelin Stefanov <39906415+ProgrammingPro27@users.noreply.github.com>
2021-12-30 19:20:46 +0200

    Update mapScripts.js

--- mapScripts.js
+++ mapScripts.js
@@ -13,8 +13,7 @@ let gameObject = {
     tileZ: 20,
     x: window.innerWidth / 2,
     y: window.innerHeight / 4,
-    activateStroke: true,
-    map: []
+    activateStroke: true
 };
 
 canvas.addEventListener("mousemove", function (e) {
@@ -32,6 +31,9 @@ function updateStroke() {
 
 let chunk = new Chunk();
 
+let perlin = new Perlin();
+perlin.seed();
+
 function updateMap(command) {
     try {
         let values = {
@@ -42,8 +44,8 @@ function updateMap(command) {
             fieldValueGroundLayers: Number(document.getElementById("groundLayers").value),
         };
         let comb = {
-            c1: `chunk.createFlatChunk(values.flatCoords,values.flatCoords,"0,0")`,
-            c2: `chunk.createFlatChunk(values.perlinCoords,values.perlinCoords,"0,0").createPerlinChunk("0,0",values.fieldValueGridSize,values.fieldValueResolution,values.fieldValueGroundLayers)`,
+            c1: `chunk.createFlatChunk(canvas,ctx,gameObject.tileW,gameObject.tileZ,window.innerWidth / 2, window.innerHeight / 4, values.flatCoords,values.flatCoords,"0,0")`,
+            c2: `chunk.createFlatChunk(canvas,ctx,gameObject.tileW,gameObject.tileZ,window.innerWidth / 2, window.innerHeight / 4, values.perlinCoords,values.perlinCoords,"0,0").createPerlinChunk(perlin,"0,0",values.fieldValueGridSize,values.fieldValueResolution,values.fieldValueGroundLayers)`
         };
         eval(comb[command]);
         updateStroke();
@@ -52,14 +54,69 @@ function updateMap(command) {
     };
 };
 
+canvas.addEventListener("mousewheel", onmousewheel, false);
+
+const view = (() => {
+    const matrix = [1, 0, 0, 1, 0, 0];
+    var m = matrix;
+    var scale = 1;
+    var ctx;
+    const pos = { x: 0, y: 0 };
+    var dirty = true;
+    const API = {
+        setContext(_ctx) { ctx = _ctx; dirty = true },
+        apply() {
+            if (dirty) { this.update() }
+            ctx.setTransform(...m)
+        },
+        getScale() { return scale },
+        getPosition() { return pos },
+        isDirty() { return dirty },
+        update() {
+            dirty = false;
+            m[3] = m[0] = scale;
+            m[2] = m[1] = 0;
+            m[4] = pos.x;
+            m[5] = pos.y;
+        },
+        scaleAt(at, amount) {
+            if (dirty) { this.update() }
+            scale *= amount;
+            pos.x = at.x - (at.x - pos.x) * amount;
+            pos.y = at.y - (at.y - pos.y) * amount;
+            dirty = true;
+        },
+    };
+    return API;
+})();
+
+view.setContext(ctx);
+
+function onmousewheel(event) {
+    var e = window.event || event;
+    var x = e.offsetX;
+    var y = e.offsetY;
+    const delta = e.type === "mousewheel" ? e.wheelDelta : -e.detail;
+    if (delta > 0) {
+        view.scaleAt({ x, y }, 1.1)
+    }
+    else {
+        view.scaleAt({ x, y }, 1 / 1.1)
+    }
+    updateStroke();
+    e.preventDefault();
+}
+
+
 function render() {
     requestAnimationFrame(render);
-    ctx.save();
+    // ctx.save();
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.restore();
+    view.apply();
+    // ctx.restore();
     canvas.style.cursor = "default";
-    chunk.loadChunk("0,0", gameObject.mouseCoordinates[0], gameObject.mouseCoordinates[1], gameObject.eventToPut, gameObject.activateStroke, gameObject.key);
+    chunk.loadChunk(`0,0`, gameObject.mouseCoordinates[0], gameObject.mouseCoordinates[1], gameObject.eventToPut, gameObject.activateStroke, gameObject.key);
 };
 
 render();
@@ -70,31 +127,33 @@ window.addEventListener("resize", function () {
 });
 
 window.addEventListener("keypress", function (e) {
-    switch (e.code) {
-        case "KeyW": ctx.translate(0, -10); break;
-        case "KeyS": ctx.translate(0, 10); break;
-        case "KeyA": ctx.translate(-10, 0); break;
-        case "KeyD": ctx.translate(10, 0); break;
-    };
+    if (chunk.mapData["0,0"].length !== 0) {
+        for (let i = 0; i < chunk.mapData["0,0"].length; i++) {
+            for (let j = 0; j < chunk.mapData["0,0"][i].length; j++) {
+                let currEl = chunk.mapData["0,0"][i][j];
+                if (currEl) {
+                    switch (e.code) {
+                        case "KeyW":
+                            currEl.y += 10;
+                            gameObject.activateStroke = false
+                                ; break;
+                        case "KeyS":
+                            currEl.y -= 10;
+                            gameObject.activateStroke = false
+                                ; break;
+                        case "KeyA":
+                            currEl.x += 10;
+                            gameObject.activateStroke = false
+                                ; break;
+                        case "KeyD":
+                            currEl.x -= 10;
+                            gameObject.activateStroke = false
+                                ; break;
+                    }
+                }
+            }
+        }
+    }
     gameObject.key = e.code;
     updateStroke();
 });
-
-function zoom(clicks) {
-    let scaleFactor = 1.1;
-    ctx.translate(gameObject.mouseCoordinates[0], gameObject.mouseCoordinates[1]);
-    let factor = Math.pow(scaleFactor, clicks);
-    ctx.scale(factor, factor);
-    ctx.translate(-gameObject.mouseCoordinates[0], -gameObject.mouseCoordinates[1]);
-};
-
-let handleScroll = function (evt) {
-    let delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
-    if (delta) {
-        zoom(delta);
-    };
-    updateStroke();
-    return evt.preventDefault() && false;
-};
-
-canvas.addEventListener('mousewheel', handleScroll, false);
