0fadcc97c7a64e24be957dad57b39c073f6b2b13
Tsvetelin Stefanov <39906415+ProgrammingPro27@users.noreply.github.com>
2022-03-10 08:05:11 +0200

    Update Chunk.js

--- Chunk.js
+++ Chunk.js
@@ -48,14 +48,22 @@ Chunk.prototype.cleanChunk = function (code) {
     y = window.innerHeight / 4;
     return this;
 };
-Chunk.prototype.createPerlinChunk = function (size, perlin, code, gridSize, resolution, groundLayers, heightLimit) {
+
+let flying = 0;
+let flying2 = 0;
+Chunk.prototype.createPerlinChunk = function (size, perlin, code, gridSize, resolution, groundLayers, heightLimit,num,op) {
     if ((gridSize / resolution) == gridSize || gridSize == resolution || (gridSize % resolution) == 0) {
-        resolution = this.mapData[code].length;
+        resolution = this.mapData[code].length
         // alert("Invalid resoulution, increase the resolution in order to continue!")
     }
-    let yoff = 0;
+    if(num,op){
+        let currentFlying = num;
+        eval(`${currentFlying} ${op}= gridSize / resolution`)
+    }
+
+    let yoff = flying2;
     for (let y = 0; y < size; y++) {
-        let xoff = 0;
+        let xoff = flying;
         for (let x = 0; x < size; x++) {
             let v = parseInt((perlin.get(xoff, yoff) + groundLayers) * heightLimit);//default 255     
             if (v < 0) {
