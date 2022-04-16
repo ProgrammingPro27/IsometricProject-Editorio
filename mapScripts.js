b7fd43cb8828889531e92e7acea63c56cd79e951
Tsvetelin Stefanov <39906415+ProgrammingPro27@users.noreply.github.com>
2022-04-16 19:23:39 +0200

    Update mapScripts.js

--- mapScripts.js
+++ mapScripts.js
@@ -96,7 +96,7 @@ function onmousewheel(event) {
 }
 
 function updatePosition(values, num, val, num1, val2) {
-    chunk.createPerlinChunk(values.perlinCoords, perlin, "0,0", values.fieldValueGridSize, values.fieldValueResolution, values.fieldValueGroundLayers, values.fieldValueHeightLimit, num, val, num1, val2);
+    chunk.createFlatChunk(canvas,ctx,gameObject.tileW,gameObject.tileZ,window.innerWidth / 2, window.innerHeight / 4, values.perlinCoords,values.perlinCoords,"0,0").createPerlinChunk(values.perlinCoords, perlin, "0,0", values.fieldValueGridSize, values.fieldValueResolution, values.fieldValueGroundLayers, values.fieldValueHeightLimit, num, val, num1, val2);
     gameObject.activateStroke = false;
 }
 
