ccf1edc4870c89cf167c901be6f31ba836f4ce37
Tsvetelin Stefanov <39906415+ProgrammingPro27@users.noreply.github.com>
2022-06-18 22:16:10 +0200

    Update IsometricTile.js

--- IsometricTile.js
+++ IsometricTile.js
@@ -1,128 +1,74 @@
-function Iso3d(canvas, ctx, x, y, wx, wy, h, flag) {
+function Iso3d(canvas, ctx, x, y, width, h) {
     this.canvas = canvas;
     this.ctx = ctx;
     this.x = x;
     this.y = y;
-    this.wx = wx;
-    this.wy = wy;
+    this.width = width;
     this.h = h;
-    this.flag = flag;
+    this.flag = false;
     this.leftColor = "#808080";
     this.rightColor = "#A9A9A9";
     this.topColor = "#909090";
-    this.cellCoordinate = { row: null, col: null };
 };
 Iso3d.prototype.drawCube = function (stroke) {
-    let sides = [{
-        moveTo: [this.x, this.y],
-        sides: [[this.x - this.wx, this.y - this.wx * 0.5], [this.x - this.wx, this.y - this.h - this.wx * 0.5], [this.x, this.y - this.h * 1]],
-        color: this.leftColor
-    }, {
-        moveTo: [this.x, this.y],
-        sides: [[this.x + this.wy, this.y - this.wy * 0.5], [this.x + this.wy, this.y - this.h - this.wy * 0.5], [this.x, this.y - this.h * 1]],
-        color: this.rightColor
-    }, {
-        moveTo: [this.x, this.y - this.h],
-        sides: [[this.x - this.wx, this.y - this.h - this.wx * 0.5], [this.x - this.wx + this.wy, this.y - this.h - (this.wx * 0.5 + this.wy * 0.5)], [this.x + this.wy, this.y - this.h - this.wy * 0.5]],
-        color: this.topColor
-    }];
-
-    for (let i = 0; i < sides.length; i++) {
-        this.ctx.beginPath();
-        this.ctx.moveTo(...sides[i].moveTo);
-        for (let j of sides[i].sides) {
-            this.ctx.lineTo(...j);
-        };
-        this.ctx.closePath();
-        this.ctx.fillStyle = sides[i].color;
-        this.ctx.fill();
+    let side = (el, p1, p2, p3, p4, p5, p6, p7, p8, p9, stroke) => {
+        el.ctx.beginPath();
+        el.ctx.moveTo(p1, p2);
+        el.ctx.lineTo(p3, p4);
+        el.ctx.lineTo(p5, p6);
+        el.ctx.lineTo(p7, p8);
+        el.ctx.closePath();
+        el.ctx.fillStyle = p9;
+        el.ctx.fill();
         if (stroke === true) {
-            this.ctx.stroke();
+            el.ctx.stroke();
         };
-    };
+    }
+    let f1 = this.width * 0.5, f2 = this.y - this.h, f3 = this.x - this.width, f4 = this.x + this.width, f5 = f2 - f1;
+    side(this, this.x, this.y, f3, this.y - f1, f3, f5, this.x, f2 * 1, this.leftColor, stroke);
+    side(this, this.x, this.y, f4, this.y - f1, f4, f5, this.x, f2 * 1, this.rightColor, stroke);
+    side(this, this.x, f2, f3, f5, f3 + this.width, f2 - f1 * 2, f4, f5, this.topColor, stroke);
     return this;
 };
-Iso3d.prototype.collision = function (map, mouseX, mouseY) {
+Iso3d.prototype.collision = function (mouseX, mouseY) {
     if (this.ctx.isPointInPath(mouseX, mouseY)) {
         this.flag = true;
-        for (let i = 0; i < map.length; i++) {
-            let index = map[i].indexOf(this);
-            if (index !== -1) {
-                this.cellCoordinate.row = i;
-                this.cellCoordinate.col = index;
-            };
-        };
-        this.canvas.style.cursor = "pointer";
-        return this;
     } else {
         this.flag = false;
-        this.cellCoordinate.row = null;
-        this.cellCoordinate.col = null;
-        return this;
-    };
-};
-Iso3d.prototype.increaseSize = function () {
-    let value = this;
-    if (value.flag === true) {
-        this.canvas.onclick = function () {
-            if (value.flag === true) {
-                value.h += 5;
-            };
-        };
     };
     return this;
 };
-Iso3d.prototype.removeTile = function (map) {
+Iso3d.prototype.eventInitializer = function (event, map, key) {
     let value = this;
     if (value.flag === true) {
         this.canvas.onclick = function () {
             if (value.flag === true) {
-                let i = value.cellCoordinate.row;
-                let j = value.cellCoordinate.col;
-                if (i !== null && j !== null) {
-                    map[i] = map[i].filter(item => item !== value);
+                let events = {
+                    increaseSize: () => {
+                        value.h += 5;
+                    },
+                    removeTile: () => {
+                        for (let i = 0; i < map.length; i++) {
+                            map[i] = map[i].filter(item => item !== value);
+                        }
+                    },
+                    colorise: () => {
+                        let colors = {
+                            Digit1: ["#FFFCFC", "#FFFBFB", "#FFFAFA"],//snow Block
+                            Digit2: ["#2389da", "#2389da", "#2389da"],//water Block
+                            Digit3: ["#4D525B", "#787F8E", "#606672"],//stone Block 
+                            Digit4: ["#cabc91", "#dbd1b4", "#d3c7a2"],//sand Block 
+                            Digit5: ["#8B4513", "#A0522D", "#6B8E23"]//grass Block 
+                        };
+                        if (colors[key]) {
+                            value.leftColor = colors[key][0];
+                            value.rightColor = colors[key][1];
+                            value.topColor = colors[key][2];
+                        };
+                    }
                 };
+                events[event]();
             };
         };
     };
-    return this;
-};
-Iso3d.prototype.addUpperLevel = function (map) {
-    let value = this;
-    if (value.flag === true) {
-        this.canvas.onclick = function () {
-            if (value.flag === true) {
-                let i = value.cellCoordinate.row;
-                let j = value.cellCoordinate.col;
-                let isoCube = new Iso3d(value.canvas, value.ctx, map[i][j].x, map[i][j].y - map[i][j].h, gameObject.tileW, gameObject.tileH, gameObject.tileZ);
-                if (i !== null && j !== null) {
-                    map[i].push(isoCube);
-                    map[i] = map[i].sort((a, b) => { return a.x - b.x || b.y - a.y });
-                };
-            };
-        };
-    };
-    return this;
-};
-Iso3d.prototype.colorise = function (key) {
-    let value = this;
-    if (value.flag === true) {
-        this.canvas.onclick = function () {
-         if (value.flag === true) {
-            let colors = {
-                Digit1: ["#FFFCFC", "#FFFBFB", "#FFFAFA"],//snow Block
-                Digit2: ["#2389da", "#2389da", "#2389da"],//water Block
-                Digit3: ["#4D525B", "#787F8E", "#606672"],//stone Block 
-                Digit4: ["#cabc91", "#dbd1b4", "#d3c7a2"],//sand Block 
-                Digit5: ["#8B4513", "#A0522D", "#6B8E23"]//grass Block 
-            };
-            if (colors[key]) {
-                value.leftColor = colors[key][0];
-                value.rightColor = colors[key][1];
-                value.topColor = colors[key][2];
-            };
-          };
-        };
-    };
-    return this;
 };
