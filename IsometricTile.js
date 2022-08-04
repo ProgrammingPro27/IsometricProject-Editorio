b210055133e496278ac043e2e58a2afca618d6dd
Tsvetelin Stefanov <39906415+ProgrammingPro27@users.noreply.github.com>
2022-08-04 22:34:53 +0200

    Update IsometricTile.js

--- IsometricTile.js
+++ IsometricTile.js
@@ -1,56 +1,54 @@
-function Iso3d(canvas, ctx, x, y, width, h) {
-    this.canvas = canvas;
+function Iso3d(ctx, x, y, width, h) {
     this.ctx = ctx;
     this.x = x;
     this.y = y;
     this.width = width;
     this.h = h;
     this.flag = false;
-    this.leftColor = "#808080";
-    this.rightColor = "#A9A9A9";
-    this.topColor = "#909090";
+    this.colors = ["#808080", "#A9A9A9", "#909090"];
 };
 Iso3d.prototype.drawCube = function (stroke) {
-    let side = (el, p1, p2, p3, p4, p5, p6, p7, p8, p9, stroke) => {
-        el.ctx.beginPath();
-        el.ctx.moveTo(p1, p2);
-        el.ctx.lineTo(p3, p4);
-        el.ctx.lineTo(p5, p6);
-        el.ctx.lineTo(p7, p8);
-        el.ctx.closePath();
-        el.ctx.fillStyle = p9;
-        if (stroke == undefined) {
-            el.ctx.stroke();
-        };
-        el.ctx.fill();
-    };
     let f1 = this.width * 0.5, f2 = this.y - this.h, f3 = this.x - this.width, f4 = this.x + this.width, f5 = f2 - f1;
-    side(this, this.x, this.y, f3, this.y - f1, f3, f5, this.x, f2 * 1, this.leftColor, stroke);
-    side(this, this.x, this.y, f4, this.y - f1, f4, f5, this.x, f2 * 1, this.rightColor, stroke);
-    side(this, this.x, f2, f3, f5, f3 + this.width, f2 - f1 * 2, f4, f5, this.topColor, stroke);
+    let sides = [[this.y, f3, this.y - f1, f3, f5, this.x, f2 * 1],
+    [this.y, f4, this.y - f1, f4, f5, this.x, f2 * 1],
+    [f2, f3, f5, f3 + this.width, f2 - f1 * 2, f4, f5]];
+    for (let i = 0; i < sides.length; i++) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.x, sides[i][0]);
+        this.ctx.lineTo(sides[i][1], sides[i][2]);
+        this.ctx.lineTo(sides[i][3], sides[i][4]);
+        this.ctx.lineTo(sides[i][5], sides[i][6]);
+        this.ctx.closePath();
+        this.ctx.fillStyle = this.colors[i];
+        if (stroke == undefined) {
+            this.ctx.stroke();
+        };
+        this.ctx.fill();
+    };
     return this;
 };
 Iso3d.prototype.collision = function (mouseX, mouseY) {
+    this.flag = false;
     if (this.ctx.isPointInPath(mouseX, mouseY)) {
         this.flag = true;
-    } else {
-        this.flag = false;
     };
     return this;
 };
 Iso3d.prototype.eventInitializer = function (event, map, key) {
     let value = this;
     if (value.flag === true) {
-        this.canvas.onclick = function () {
+        this.ctx.canvas.onclick = function () {
             if (value.flag === true) {
                 let events = {
                     increaseSize: () => {
                         value.h += 5;
                     },
                     removeTile: () => {
-                        for (let i = 0; i < map.length; i++) {
-                            map[i] = map[i].filter(item => item !== value);
-                        }
+                        if (value.h - 50 < 0) {
+                            value.h = 0;
+                        } else {
+                            value.h -= 50;
+                        };
                     },
                     colorise: () => {
                         let colors = {
@@ -61,9 +59,7 @@ Iso3d.prototype.eventInitializer = function (event, map, key) {
                             Digit5: ["#8B4513", "#A0522D", "#6B8E23"]//grass Block 
                         };
                         if (colors[key]) {
-                            value.leftColor = colors[key][0];
-                            value.rightColor = colors[key][1];
-                            value.topColor = colors[key][2];
+                            value.colors = colors[key];
                         };
                     }
                 };
