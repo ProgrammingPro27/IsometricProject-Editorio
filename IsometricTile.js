ba56b6dd9a6135f8ef68dc2bf67f9d0bbff226fe
Tsvetelin Stefanov <39906415+ProgrammingPro27@users.noreply.github.com>
2022-08-05 10:10:45 +0200

    Update IsometricTile.js

--- IsometricTile.js
+++ IsometricTile.js
@@ -1,5 +1,4 @@
-function Iso3d(ctx, x, y, width, h) {
-    this.ctx = ctx;
+function Iso3d(x, y, width, h) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -7,37 +6,37 @@ function Iso3d(ctx, x, y, width, h) {
     this.flag = false;
     this.colors = ["#808080", "#A9A9A9", "#909090"];
 };
-Iso3d.prototype.drawCube = function (stroke) {
+Iso3d.prototype.drawCube = function (ctx, stroke) {
     let f1 = this.width * 0.5, f2 = this.y - this.h, f3 = this.x - this.width, f4 = this.x + this.width, f5 = f2 - f1;
     let sides = [[this.y, f3, this.y - f1, f3, f5, this.x, f2 * 1],
     [this.y, f4, this.y - f1, f4, f5, this.x, f2 * 1],
     [f2, f3, f5, f3 + this.width, f2 - f1 * 2, f4, f5]];
     for (let i = 0; i < sides.length; i++) {
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.x, sides[i][0]);
-        this.ctx.lineTo(sides[i][1], sides[i][2]);
-        this.ctx.lineTo(sides[i][3], sides[i][4]);
-        this.ctx.lineTo(sides[i][5], sides[i][6]);
-        this.ctx.closePath();
-        this.ctx.fillStyle = this.colors[i];
+        ctx.beginPath();
+        ctx.moveTo(this.x, sides[i][0]);
+        ctx.lineTo(sides[i][1], sides[i][2]);
+        ctx.lineTo(sides[i][3], sides[i][4]);
+        ctx.lineTo(sides[i][5], sides[i][6]);
+        ctx.closePath();
+        ctx.fillStyle = this.colors[i];
         if (stroke == undefined) {
-            this.ctx.stroke();
+            ctx.stroke();
         };
-        this.ctx.fill();
+        ctx.fill();
     };
     return this;
 };
-Iso3d.prototype.collision = function (mouseX, mouseY) {
+Iso3d.prototype.collision = function (ctx, mouseX, mouseY) {
     this.flag = false;
-    if (this.ctx.isPointInPath(mouseX, mouseY)) {
+    if (ctx.isPointInPath(mouseX, mouseY)) {
         this.flag = true;
     };
     return this;
 };
-Iso3d.prototype.eventInitializer = function (event, key) {
+Iso3d.prototype.eventInitializer = function (ctx, event, key) {
     let value = this;
     if (value.flag === true) {
-        this.ctx.canvas.onclick = function () {
+        ctx.canvas.onclick = function () {
             if (value.flag === true) {
                 let events = {
                     increaseSize: () => {
