86d9eb7e257ccee52e9839069cac56440c0c6b3e
Tsvetelin Stefanov <39906415+ProgrammingPro27@users.noreply.github.com>
2022-08-10 22:54:56 +0200

    Update contentMethods.js

--- contentMethods.js
+++ contentMethods.js
@@ -4,34 +4,34 @@ let mapOptionsWindow = document.getElementById("optionPartsMenu");
 let operations = ["removeTile", "increaseSize", "colorise"];
 
 let mapOperations = {
-    "RowsAndColumns": () => {
-        let optionFragment = `<div class = "part" id ="RowsAndColumns">`;
-        optionFragment += `<label for="quantity1">Flat Map Size</label><input type="range" id="quantity1" name="quantity1" min="1" max="50" value = "20" oninput="updateMap('c1')">`;
+    "MapMode": () => {
+        let optionFragment = `<div class = "part" id ="MapMode">`;
+        optionFragment += `<button id = "mode" onclick = "mapType(this)">Perlin</button>`;
         return optionFragment;
     },
-    "PerlinMapRowsAndColumns": () => {
-        let optionFragment = `<div class = "part" id ="PerlinMapRowsAndColumns">`;
-        optionFragment += `<label for="quantity3">Perlin Map Size</label><input  type="range" id="quantity3" name="quantity3" min="1" max="50" value = "20" oninput="updateMap('c3')">`;
+    "RowsAndColumns": () => {
+        let optionFragment = `<div class = "part" id ="RowsAndColumns">`;
+        optionFragment += `<label for="quantity1">Chunk Size</label><input type="range" id="quantity1" name="quantity1" min="1" max="50" value = "20" oninput="updateMap('make')">`;
         return optionFragment;
     },
     "GridSize": () => {
         let optionFragment = `<div class = "part" id ="GridSize">`;
-        optionFragment += `<label for="gridSize">Grid Size</label><input  type="number" id="gridSize" name="gridSize" min="1" value = "8" oninput="updateMap('c2')">`;
+        optionFragment += `<label for="gridSize">Grid Size</label><input  type="number" id="gridSize" name="gridSize" min="1" value = "8" oninput="updateMap('edit')">`;
         return optionFragment;
     },
     "Resolution": () => {
         let optionFragment = `<div class = "part" id ="Resolution">`;
-        optionFragment += `<label for="resolution">Resolution</label><input  type="number" id="resolution" name="resolution" min="1" value = "64" oninput="updateMap('c2')">`;
+        optionFragment += `<label for="resolution">Resolution</label><input  type="number" id="resolution" name="resolution" min="1" value = "64" oninput="updateMap('edit')">`;
         return optionFragment;
     },
     "GroundLayers": () => {
         let optionFragment = `<div class = "part" id ="GroundLayers">`;
-        optionFragment += `<label for="groundLayers">Ground Layers</label><input  type="number" id="groundLayers" name="groundLayers" min="0.1" value = "0.5" step = "0.1" oninput="updateMap('c2')">`;
+        optionFragment += `<label for="groundLayers">Ground Layers</label><input  type="number" id="groundLayers" name="groundLayers" min="0.1" value = "0.5" step = "0.1" oninput="updateMap('edit')">`;
         return optionFragment;
     },
     "HeightLimit": () => {
         let optionFragment = `<div class = "part" id ="HeightLimit">`;
-        optionFragment += `<label for="heightLimit">Height Limit</label><input  type="number" id="heightLimit" name="heightLimit" min="1" value = "1000" step = "1" oninput="updateMap('c2')">`;
+        optionFragment += `<label for="heightLimit">Height Limit</label><input  type="number" id="heightLimit" name="heightLimit" min="1" value = "1000" step = "1" oninput="updateMap('edit')">`;
         return optionFragment;
     }
 };
@@ -72,3 +72,20 @@ function showElement(button, el) {
         document.getElementById('controlWindowButton').style.display = "block";
     });
 })
+
+function mapType(el) {
+    let el1 = document.getElementById("gridSize")
+    let el2 = document.getElementById("resolution")
+    let el3 = document.getElementById("groundLayers")
+    let el4 = document.getElementById("heightLimit")
+    if (el.innerText == "Flat") {
+        el.innerText = "Perlin";
+        el.style.backgroundColor = "#ffa000";
+        el1.disabled = el2.disabled = el3.disabled = el4.disabled = false;
+    } else {
+        el.innerText = "Flat";
+        el.style.backgroundColor = "gray";
+        el1.disabled = el2.disabled = el3.disabled = el4.disabled = true;
+    }
+    updateMap("make");
+};
